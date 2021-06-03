11c001aa479b30b345a7f5a8cd37474bcbbb257e
Aponyo <fred.aponyo@lanstar.co.ke>
2021-06-03 15:09:35 +0300

    .

---
+++ src/main/java/com/shabarecords/farmersmodule/configuration/CorsConfiguration.java
@@ -0,0 +1,24 @@
+package com.shabarecords.farmersmodule.configuration;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@EnableConfigurationProperties
+public class CorsConfiguration implements WebMvcConfigurer
+{
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        long MAX_AGE_SECS = 3600;
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods( "GET", "POST", "PUT", "DELETE")
+                
+                .maxAge(MAX_AGE_SECS);
+    }
+}
